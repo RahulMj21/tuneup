68522ac1a07e88a3abe1e6fc4ae15ce987d15ea8
Rahul Mondal <rahulmondar@gmail.com>
2023-05-03 10:27:09 +0530

    feat: added animated navbar

---
+++ src/components/navigation/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <a href="#" className="cursor-pointer">
+            <h1>Logo</h1>
+        </a>
+    );
+};
+
+export default Logo;
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const LINKS = [
+    { text: "HOME", href: "/" },
+    { text: "DASHBOARD", href: "/dashboard" },
+    { text: "Support", href: "/support" },
+    { text: "Contact", href: "/contact" },
+];
