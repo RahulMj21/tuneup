5e56ec637f5e239b911296496c49b42fb18243fb
Rahul Mondal <77981238+RahulMj21@users.noreply.github.com>
2023-05-18 10:44:52 +0530

    fix: hero section styling (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import { FaArrowRight } from "react-icons/fa";
 
 const HomePage = () => {
     return (
-        <section title="Home" className="relative w-full py-16 ">
+        <section title="Home" className="relative w-full pt-16 ">
             <div className="absolute right-0 top-0 -z-40 h-[20vw] w-[20vw] bg-gradient-to-r from-color-blue-1 to-pink-500 blur-[4.5vw] rounded-full" />
             <div className="absolute left-0 bottom-[2rem] -z-40 h-[14vw] w-[14vw] bg-gradient-to-r  from-pink-700 to-color-blue-1 blur-[5.5vw] rounded-full" />
             <div className="w-full custom-container gap-8 flex flex-col items-center">
                 <div>
-                    <h1 className="hero-heading">TAKE CONTROL</h1>
-                    <h1 className="hero-heading text-right">OF YOUR DAY</h1>
+                    <h1 className="hero-heading text-center lg:text-left">
+                        TAKE CONTROL
+                    </h1>
+                    <h1 className="hero-heading text-center lg:text-right">
+                        OF YOUR DAY
+                    </h1>
                 </div>
-                <p className="text-[1.2rem]">
+                <p className="text-[1.2rem] text-center">
                     Utilize each day with the best Screen Time software
                     available globally.
                 </p>
