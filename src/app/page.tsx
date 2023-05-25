122833f1d2a2de6022481952f01aa4c63bb7c61f
Rahul Mondal <rahulmondar@gmail.com>
2023-05-25 10:09:37 +0530

    fix: removed unwanted fragments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,30 @@ import { FaArrowRight } from "react-icons/fa";
 
 const HomePage = () => {
     return (
-        <>
-            <section title="Home" className="relative w-full pt-16 ">
-                <div className="w-full custom-container gap-8 flex flex-col items-center">
-                    <div>
-                        <h1 className="hero-heading text-center lg:text-left">
-                            TAKE CONTROL
-                        </h1>
-                        <h1 className="hero-heading text-center lg:text-right">
-                            OF YOUR DAY
-                        </h1>
-                    </div>
-                    <p className="text-[1.2rem] text-center">
-                        Utilize each day with the best Screen Time software
-                        available globally.
-                    </p>
-                    <Link
-                        href="/"
-                        className="btn group text-[1.25rem]"
-                        style={{ paddingInline: "1.5rem" }}
-                    >
-                        Get Started
-                        <FaArrowRight className="text-[1.15rem] custom-button-svg" />
-                    </Link>
+        <section title="Home" className="relative w-full pt-16 ">
+            <div className="w-full custom-container gap-8 flex flex-col items-center">
+                <div>
+                    <h1 className="hero-heading text-center lg:text-left">
+                        TAKE CONTROL
+                    </h1>
+                    <h1 className="hero-heading text-center lg:text-right">
+                        OF YOUR DAY
+                    </h1>
                 </div>
-            </section>
-        </>
+                <p className="text-[1.2rem] text-center">
+                    Utilize each day with the best Screen Time software
+                    available globally.
+                </p>
+                <Link
+                    href="/"
+                    className="btn group text-[1.25rem]"
+                    style={{ paddingInline: "1.5rem" }}
+                >
+                    Get Started
+                    <FaArrowRight className="text-[1.15rem] custom-button-svg" />
+                </Link>
+            </div>
+        </section>
     );
 };
 
