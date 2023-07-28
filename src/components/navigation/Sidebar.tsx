640b57ac6336f399376e01f92e891a913c413aa8
Rahul Mondal <rahulmondar@gmail.com>
2023-07-28 11:03:04 +0530

    fix: z-index issue of sidebar

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
     return (
         <div
             className={cn(
-                "z-[9999] border-r border-gray-5 h-100vh py-6 px-1 w-16 fixed top-0 left-0 h-[100vh] flex flex-col items-center gap-14",
+                "z-[50] border-r border-gray-5 h-100vh py-6 px-1 w-16 fixed top-0 left-0 h-[100vh] flex flex-col items-center gap-14",
                 className
             )}
             {...props}
