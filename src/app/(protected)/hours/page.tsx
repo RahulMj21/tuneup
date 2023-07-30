102ab1edb3d64780c0c2c6a9d0bc2c064838c196
Rahul Mondal <rahulmondar@gmail.com>
2023-07-30 10:56:02 +0530

    feat: added hours page header

--- src/app/(protected)/hours/page.tsx
+++ src/app/(protected)/hours/page.tsx
@@ -1,9 +1,11 @@
+import HoursHeader from "@/scenes/hours/HoursHeader";
+
 export const runtime = "edge";
 
 const HoursPage = () => {
     return (
         <section title="hours" className="px-4 py-6">
-            <h1>Hours</h1>
+            <HoursHeader />
         </section>
     );
 };
