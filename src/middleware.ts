c66751d70ac1908839b3e83034675f3dc3e1fb2c
Rahul Mondal <77981238+RahulMj21@users.noreply.github.com>
2023-09-04 21:43:39 +0530

    fix: auth error (#61)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+    debug: true,
+    afterAuth(auth, req) {
+        if (!auth.userId && !auth.isPublicRoute) {
+            return redirectToSignIn({ returnBackUrl: req.url });
+        }
+    },
+});
 
 export const config = {
     matcher: [
