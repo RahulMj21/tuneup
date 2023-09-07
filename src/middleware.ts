e92c43315e7c981e271ad05cf1c82dd16bd136f3
Rahul Mondal <rahulmondar@gmail.com>
2023-09-07 20:24:38 +0530

    fix: login redirection issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,35 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+// import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
-export default authMiddleware({
-    debug: true,
-    afterAuth(auth, req) {
-        if (!auth.userId && !auth.isPublicRoute) {
-            return redirectToSignIn({ returnBackUrl: req.url });
-        }
-    },
-});
+// export default authMiddleware({
+//     debug: true,
+//     afterAuth(auth, req) {
+//         if (!auth.userId && !auth.isPublicRoute) {
+//             return redirectToSignIn({ returnBackUrl: req.url });
+//         }
+//     },
+// });
+
+// export const config = {
+//     matcher: [
+//         "/boards",
+//         "/boards/(.*)",
+//         "/chat",
+//         "/chat/(.*)",
+//         "/dashboard",
+//         "/events",
+//         "/hours",
+//         "/settings",
+//         "/team",
+//     ],
+// };
+
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({});
 
 export const config = {
-    matcher: [
-        "/boards",
-        "/boards/(.*)",
-        "/chat",
-        "/chat/(.*)",
-        "/dashboard",
-        "/events",
-        "/hours",
-        "/settings",
-        "/team",
-    ],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
