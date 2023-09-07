a68c42cf685571195e3086b0c5c558444af83919
Rahul Mondal <77981238+RahulMj21@users.noreply.github.com>
2023-09-07 20:50:50 +0530

    fix: added all the public route (#65)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,8 @@
-// import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-
-// export default authMiddleware({
-//     debug: true,
-//     afterAuth(auth, req) {
-//         if (!auth.userId && !auth.isPublicRoute) {
-//             return redirectToSignIn({ returnBackUrl: req.url });
-//         }
-//     },
-// });
-
-// export const config = {
-//     matcher: [
-//         "/boards",
-//         "/boards/(.*)",
-//         "/chat",
-//         "/chat/(.*)",
-//         "/dashboard",
-//         "/events",
-//         "/hours",
-//         "/settings",
-//         "/team",
-//     ],
-// };
-
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/"],
+    publicRoutes: ["/", "/pricing", "/support", "/contact"],
+    ignoredRoutes: ["/", "/pricing", "/support", "/contact"],
 });
 
 export const config = {
